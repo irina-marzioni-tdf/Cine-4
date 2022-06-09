65b3e866bdb475e6b0f3d2e173e5ff36238deec2
irina-marzioni-tdf <104698059+irina-marzioni-tdf@users.noreply.github.com>
2022-06-09 12:41:50 -0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,34 @@
 let vacias: number = 0;
 let ocupadas: number = 0;
-let indice: number;
-
 let cantButacas: number;
 cantButacas = Number(prompt("Ingrese la cantidad de butacas")); //pide al usuario ingresar cantidades de butacas
 
-let butacas: number[] = new Array(cantButacas); //defino el array
+let butacas: boolean[] = new Array(cantButacas); //defino el array
 
 //----------------------------------------------
 //funcion para cargar el arreglo con booleanos
-function cargarArrayRandomBoolean(v: number, cantButacas: number) {
+function cargarArrayRandomBoolean(vector: boolean[], cantButacas: number) {
   for (let indice = 0; indice < cantButacas; indice++) {
-    v[indice] = Boolean(Math.floor(Math.random() * 2));
+    vector[indice] = Boolean(Math.floor(Math.random() * 2));
   }
-  return v;
+  return vector;
 }
 //-------------------------------------
 
-console.log(cargarArrayRandomBoolean(butacas, cantButacas));//muestra el arreglo cargado
+console.log(cargarArrayRandomBoolean(butacas, cantButacas)); //muestra el arreglo cargado
 
 //-------------------------------------------
 //funcion que verifica las cantidades de asientos ocupados y vacios
-function verificarButacas (cargarArrayRandomBoolean():number){
-for (indice = 0; indice < cantButacas; indice++) {
-  if (butacas[indice] == 1) {
-    ocupadas += 1;
-  } else {
-    vacias += 1;
+function verificarButacas(vector: boolean[]) {
+  for (let indice = 0; indice < cantButacas; indice++) {
+    if (vector[indice] === true) {
+      ocupadas += 1;
+    } else {
+      vacias += 1;
+    }
   }
 }
+//---------------------------------------------
+verificarButacas(butacas);
 console.log("Butacas ocupadas: " + ocupadas);
 console.log("Butacas vacias: " + vacias);
-}
-//---------------------------------------------
-verificarButacas(cargarArrayRandomBoolean);
